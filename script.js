6f900ad6519dedbfb974ebaef8c1b5026cbec076
ianvei <iamianveilleux@gmail.com>
2022-03-08 13:22:41 -0700

    testing some JSevent listeners

---
+++ script.js
@@ -0,0 +1,22 @@
+const buttons = document.querySelectorAll('button');
+const actionBox = document.querySelector('.action-box')
+
+click_counter = 0
+buttons.forEach((button) => {
+    button.addEventListener('click', () =>{
+        
+        // alert(button.id);
+        click_counter++
+        if (click_counter % 2 === 0) {
+            actionBox.style.background ='#3882F6';
+        }
+        else if (click_counter % 5 === 0) {
+            console.log('hello')
+            alert('Secret found!')
+        }
+        else {
+            actionBox.style.background ='#1F2937';
+        }
+        console.log(click_counter)
+    })
+})
